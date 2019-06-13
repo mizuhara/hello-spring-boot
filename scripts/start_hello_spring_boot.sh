a4e4744ed3e00059fb3a82a1f942b0d1e3dbeee7
MasayaMizuhara <MasayaMizuhara@yahoo.co.jp>
2019-06-13 21:45:50 +0900

    commit sample project for aws codedeploy.

---
+++ scripts/start_hello_spring_boot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# java -jar /home/ec2-user/hello-spring-boot-0.0.1-SNAPSHOT.jar
+echo "hello spring boot."
---
+++ src/test/java/com/example/hellospringboot/HelloSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.hellospringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = com.example.hellospringboot.HelloSpringBootApplication.class)
+public class HelloSpringBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
